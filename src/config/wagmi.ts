e2d24779fa359626f6026c4db7fb3083769a9699
Laplaceman <74650892+LaplaceMan@users.noreply.github.com>
2026-01-27 10:42:23 +0800

    fix: wallets

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,5 +1,18 @@
-
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import {
+    rainbowWallet,
+    walletConnectWallet,
+    metaMaskWallet,
+    baseAccount,
+    tokenPocketWallet,
+    trustWallet,
+    okxWallet,
+    bitgetWallet,
+    imTokenWallet,
+    safeWallet,
+    binanceWallet,
+    bybitWallet
+} from '@rainbow-me/rainbowkit/wallets';
 import { bsc } from 'wagmi/chains';
 import { http } from 'wagmi';
 
@@ -10,4 +23,28 @@ export const config = getDefaultConfig({
     transports: {
         [bsc.id]: http(),
     },
+    wallets: [
+        {
+            groupName: 'Recommended',
+            wallets: [
+                metaMaskWallet,
+                rainbowWallet,
+                walletConnectWallet,
+                baseAccount,
+            ],
+        },
+        {
+            groupName: 'Others',
+            wallets: [
+                trustWallet,
+                tokenPocketWallet,
+                okxWallet,
+                bitgetWallet,
+                imTokenWallet,
+                safeWallet,
+                binanceWallet,
+                bybitWallet
+            ],
+        },
+    ],
 });
